41dd01bd360286eff414037318566f536bc37d16
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2022-03-13 21:49:32 +0100

    corrected header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
         <div className="sm:grid sm:grid-cols-2 gap-8">
           <div>
             <h5 className="font-bold">About</h5>
-            <p>ApexLegendsMap.com is a simple app that lets you track what the current and next map is for every game-mode across Apex Legends: Pubs, ranked, arenas, and arenas ranked.</p>
+            <p className="text-sm text-slate-700">Track the current and next map is for every game-mode across Apex Legends. Pubs, ranked, arenas, ranked arenas, and eventually control: we've got it all.</p>
           </div>
-          <div>
+          <div class="mt-8 sm:mt-0">
             <h5 className="font-bold">Get in touch</h5>
-            <p className="text-sm">Got a suggestion? Reach out via <a href="mailto:apexlegendsmap@gmail.com" rel="noreferrer" className="underline">email</a>.</p>
+            <p className="text-sm text-slate-700">Got a suggestion? Reach out via <a href="mailto:apexlegendsmap@gmail.com" rel="noreferrer" className="underline">email</a>.</p>
           </div>
         </div>
       </div>
